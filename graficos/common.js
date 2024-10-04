d5c8a6c3d81ed0819f42e37301eb7d25e3133bca
jão <110256135+jaovit@users.noreply.github.com>
2024-10-04 08:04:43 -0300

    Add files via upload

--- graficos/common.js
+++ graficos/common.js
@@ -1,3 +1,10 @@
 const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyValue(variavel)
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
 }
